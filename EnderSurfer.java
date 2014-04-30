bca462627ffc55010d282fc05116a281999ba38f
Lazini <Lazini@users.noreply.github.com>
2014-04-30 17:40:41 +0300

    EnderSurfer.java

---
+++ EnderSurfer.java
@@ -0,0 +1,120 @@
+package com.lazini;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EnderPearl;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+public class EnderSurfer extends JavaPlugin implements Listener {
+
+	EnderPearl enderpearl;
+	Player shooter;
+	int health = getConfig().getInt("half-hearts");
+	String strHealth = Integer.toString(health);
+
+	int velMult = getConfig().getInt("vel-mult");
+	String strVelMult = Integer.toString(velMult);
+
+	ArrayList<Player> list = new ArrayList<Player>(100);
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		getServer().getPluginManager().registerEvents(this, this);
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+		HandlerList.unregisterAll();
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("sethearts")) {
+			if (args.length == 0) {
+				sender.sendMessage(ChatColor.GREEN
+						+ strHealth
+						+ " half hearts will be lost when someone hits the ground!");
+				return true;
+			} else if (args.length > 1) {
+				sender.sendMessage(ChatColor.RED
+						+ "Too many arguments, don't you think?");
+				return false;
+			} else {
+				reloadConfig();
+				health = Integer.parseInt(args[0]);
+				getConfig().set("half-hearts", health);
+				strHealth = Integer.toString(health);
+				saveConfig();
+				sender.sendMessage(ChatColor.GREEN
+						+ " Configuration file updated!" + ChatColor.GOLD
+						+ " (half-hearts: " + strHealth + ")");
+				return true;
+			}
+		} else if (command.getName().equalsIgnoreCase("setVelocityMultiplier")) {
+			if (args.length == 0) {
+				sender.sendMessage(ChatColor.GREEN
+						+ "The current velocity multiplier is " + strVelMult);
+				return true;
+			} else if (args.length > 1) {
+				sender.sendMessage(ChatColor.RED
+						+ "Too many arguments, don't you think?");
+				return false;
+			} else {
+				reloadConfig();
+				velMult = Integer.parseInt(args[0]);
+				getConfig().set("vel-mult", velMult);
+				strVelMult = Integer.toString(velMult);
+				saveConfig();
+				sender.sendMessage(ChatColor.GREEN
+						+ " Configuration file updated!" + ChatColor.GOLD
+						+ " (vel-mult: " + strVelMult + ")");
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	@EventHandler
+	public void onThrowEnderPearl(ProjectileLaunchEvent event) {
+		if (!(event.getEntity().getShooter() instanceof Player)
+				|| !(event.getEntity() instanceof EnderPearl))
+			return;
+		shooter = (Player) event.getEntity().getShooter();
+		Vector velocity = event.getEntity().getVelocity();
+		velocity = velocity.multiply(velMult);
+
+		shooter.setVelocity(velocity);
+		event.getEntity().remove();
+		list.add(shooter);
+	}
+
+	@EventHandler
+	public void onGetDamage(EntityDamageEvent event) {
+		if (!(event.getEntity() instanceof Player)
+				|| event.getEntity() != shooter)
+			return;
+		// && event.getEntity().getEntityId() == shooter.getEntityId()
+		if (event.getCause() == DamageCause.FALL && list.contains(shooter)) {
+			shooter.setHealth(shooter.getHealth() - health);
+			list.remove(shooter);
+			event.setCancelled(true);
+		}
+	}
+}
