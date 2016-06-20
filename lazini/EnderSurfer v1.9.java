19fef88a2125b98fb18644bacdff7ba38260dcf6
Lazini <Lazini@users.noreply.github.com>
2016-06-20 21:49:38 +0300

    Updated code to EnderSurfer v1.10
    Reviewed some code and fixed the not-so-smooth surfing. 
    Changed the way particles are spawned.
    Implemented elytra & EnderSurfer correctly.

--- lazini/EnderSurfer v1.9.java
+++ lazini/EnderSurfer v1.9.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import java.util.UUID;
 
 import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.EnderPearl;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -31,6 +32,7 @@ public class EnderSurfer extends JavaPlugin implements Listener {
 	public HashSet<UUID> list;
 	public HashSet<UUID> threwE;
 	public HashSet<UUID> particles;
+	private List<String> disabledWorlds;
 
 	@Override
 	public void onEnable() {
@@ -50,54 +52,61 @@ public class EnderSurfer extends JavaPlugin implements Listener {
 		list = new HashSet<>();
 		threwE = new HashSet<>();
 		particles = new HashSet<>();
+		disabledWorlds = this.getConfig().getStringList("disabled-for-worlds");
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+		list.clear();
+		threwE.clear();
+		particles.clear();
+		disabledWorlds.clear();
 	}
 
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onThrowEnderPearl(ProjectileLaunchEvent event) {
-		if (!(event.getEntity().getShooter() instanceof Player)
-				|| !(event.getEntity() instanceof EnderPearl)
-				|| !((Player) event.getEntity().getShooter())
-						.hasPermission("endersurfer.utilize")) {
+		if (!(event.getEntity().getShooter() instanceof Player) || !(event.getEntity() instanceof EnderPearl)
+				|| !((Player) event.getEntity().getShooter()).hasPermission("endersurfer.utilize")) {
 			return;
 		}
 		Player shooter = (Player) event.getEntity().getShooter();
 		UUID shooterUUID;
-
-		if (shooter.isSneaking())
+		if (shooter.isSneaking() || ((shooter.isGliding() && !shooter.hasPermission("endersurfer.elytra"))))
 			return;
-		if (this.getConfig().contains("disabled-for-worlds") && this.getConfig().isList("disabled-for-worlds")) { 
-			List<String> disabledWorlds = this.getConfig().getStringList("disabled-for-worlds");
-			for (String str : disabledWorlds) { 
-				if (shooter.getWorld().getName().equalsIgnoreCase(str)) {
-					return;
-				}
-			}
 
+		for (String str : disabledWorlds) {
+			if (shooter.getWorld().getName().equalsIgnoreCase(str)) {
+				// shooter.sendMessage("Disalbed for world: " + str);
+				return;
+			}
 		}
 
 		if (shooter.isOnGround()) {
 			Location endermiteLoc = event.getEntity().getLocation();
 			if (random.nextInt(100) < 5) {
-				event.getEntity().getWorld()
-						.spawnEntity(endermiteLoc, EntityType.ENDERMITE);
+				event.getEntity().getWorld().spawnEntity(endermiteLoc, EntityType.ENDERMITE);
 			}
 		}
 
-		this.reloadConfig();
-		health = getConfig().getInt("half-hearts");
-		velMult = getConfig().getInt("vel-mult");
-		dmgOnAir = getConfig().getBoolean("dmg-on-air");
-		showParticles = getConfig().getBoolean("show-particles");
-		Vector velocity = event.getEntity().getVelocity();
+//		this.reloadConfig();
+//		health = getConfig().getInt("half-hearts");
+//		velMult = getConfig().getInt("vel-mult");
+//		dmgOnAir = getConfig().getBoolean("dmg-on-air");
+//		showParticles = getConfig().getBoolean("show-particles");
+		Vector velocity = event.getEntity().getLocation().getDirection();
 
 		if (shooter.isOnGround() && (shooter.getLocation().getPitch() < 15)) {
 			Location loc = shooter.getLocation();
-			loc.setY(loc.getY() + .2);
+			loc.setY(loc.getY() + .5);
 			shooter.teleport(loc);
 		}
 
-		velocity = velocity.multiply(velMult);
+		velocity = velocity.setY(-velocity.getY() * velMult);
+		velocity = velocity.setX(-velocity.getX() * velMult);
+		velocity = velocity.setZ(velocity.getZ() * velMult);
+//		velocity = velocity.multiply(velMult);
 		shooter.setVelocity(velocity);
 		event.getEntity().remove();
 		shooterUUID = shooter.getUniqueId();
@@ -112,10 +121,10 @@ public class EnderSurfer extends JavaPlugin implements Listener {
 				threwE.add(shooterUUID);
 		}
 
-		if (!particles.contains(shooterUUID)
-				&& (showParticles && shooter
-						.hasPermission("endersurfer.showparticles"))) {
+		if (!particles.contains(shooterUUID) && (showParticles && shooter.hasPermission("endersurfer.showparticles"))) {
+			// shooter.sendMessage(ChatColor.BLUE + "Added to the particles list.");
 			particles.add(shooterUUID);
+			// shooter.sendMessage("Should now be added to the 'particles' list via throw enderpearl");
 		}
 	}
 
@@ -123,13 +132,13 @@ public class EnderSurfer extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onGetDamage(EntityDamageEvent event) {
 		if (!(event.getEntity() instanceof Player)
-				|| !((Player) event.getEntity())
-						.hasPermission("endersurfer.utilize"))
+		//	|| event.getEntity() != shooter
+				|| !((Player) event.getEntity()).hasPermission("endersurfer.utilize"))
 			return;
 		Player shooter = (Player) event.getEntity();
 		UUID shooterUUID = shooter.getUniqueId();
 
-		health = getConfig().getInt("half-hearts");
+//		health = getConfig().getInt("half-hearts");
 		if (event.getCause() == DamageCause.FALL) {
 			if (list.contains(shooterUUID)) {
 				list.remove(shooterUUID);
@@ -143,14 +152,13 @@ public class EnderSurfer extends JavaPlugin implements Listener {
 				event.setCancelled(true);
 			}
 			if (particles.contains(shooterUUID)) {
+				// shooter.sendMessage(ChatColor.BLUE + "Removed from the particles list.");
 				particles.remove(shooterUUID);
+				// shooter.sendMessage("Should now be removed from the 'particles' list via get Damage");
 				if (shooter.isOnGround()) {
 					Location endermiteLoc = event.getEntity().getLocation();
 					if (random.nextInt(100) < 5) {
-						event.getEntity()
-								.getWorld()
-								.spawnEntity(endermiteLoc, EntityType.ENDERMITE);
-
+						event.getEntity().getWorld().spawnEntity(endermiteLoc, EntityType.ENDERMITE);
 					}
 				}
 			}
@@ -161,15 +169,12 @@ public class EnderSurfer extends JavaPlugin implements Listener {
 	@SuppressWarnings("deprecation")
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onPlayerFlyWithEnderPearl(PlayerMoveEvent event) {
-		if (!(event.getPlayer() instanceof Player)
-				|| !(event.getPlayer()).hasPermission("endersurfer.utilize"))
+		if (!(event.getPlayer() instanceof Player) || !(event.getPlayer()).hasPermission("endersurfer.utilize"))
 			return;
-		Player shooter;
-		UUID shooterUUID;
-		shooter = event.getPlayer();
-		shooterUUID = shooter.getUniqueId();
-		reloadConfig();
-		showParticles = getConfig().getBoolean("show-particles");
+		Player shooter = event.getPlayer();
+		UUID shooterUUID = shooter.getUniqueId();
+//		reloadConfig();
+//		showParticles = getConfig().getBoolean("show-particles");
 
 		if (particles.contains(shooterUUID)) {
 			if (shooter.isOnGround()) {
@@ -177,16 +182,21 @@ public class EnderSurfer extends JavaPlugin implements Listener {
 				if (tempVel.getX() < 0.1 && tempVel.getX() > -0.1) {
 					if (tempVel.getZ() < 0.1 && tempVel.getZ() > -0.1) {
 						// if (tempVel.getY())
-						if (shooter.getLocation().getPitch() > -10
-								&& shooter.getLocation().getPitch() <= 90) {
+						if (shooter.getLocation().getPitch() > -10 && shooter.getLocation().getPitch() <= 90) {
+							/*if (threwE.contains(shooterUUID))
+								threwE.remove(shooterUUID);
+							else if (list.contains(shooterUUID))
+								list.remove(shooterUUID);
+							*/
 							particles.remove(shooterUUID);
+							// shooter.sendMessage("Move method deleted you");
 							return;
 						}
 					}
 				}
 			}
-			if (shooter.hasPermission("endersurfer.showparticles")
-					&& showParticles) {
+			if (shooter.hasPermission("endersurfer.showparticles") && showParticles) {
+				// shooter.sendMessage(ChatColor.BLUE + "Showing particles for the player.");
 				showEnderParticles(shooter);
 			}
 		}
@@ -205,6 +215,7 @@ public class EnderSurfer extends JavaPlugin implements Listener {
 	private void showEnderParticles(Player shooter) {
 		Location loc = shooter.getLocation();
 		loc.setY(loc.getY() + 1);
-		ParticleEffect.PORTAL.display(.5f, .5f, .5f, .5f, 20, loc, 40);
+//		ParticleEffect.PORTAL.display(.5f, .5f, .5f, .5f, 20, loc, 40);
+		shooter.spawnParticle(Particle.PORTAL, loc, 70);
 	}
 }
